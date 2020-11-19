79feb7cf27c6d6e0a26836e90190caf487460473
Masoud Azizi <mablue92@gmail.com>
2020-11-19 08:58:18 +0330

    Update ttc.py
    Split just spaces, excluding new lines

--- ttc.py
+++ ttc.py
@@ -1,11 +1,11 @@
 # TTC: Twitter text compressing script
 # developer: Masoud Azizi
 # Email: mablue92@gmail.com
-i0="masoud azizi\nJafar AzizI \n amir mohammad AZIZI"
+i0="masoud azizi\nJafar AzizI \n amir mohammad AZIZI."
 print(i0)
 
 i1=i0.lower()
-o0=i1.split()
+o0=i1.split(" ")
 print(o0)
 
 o2=[o1.capitalize() for o1 in o0]
