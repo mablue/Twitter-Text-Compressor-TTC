3f08ae5b0336f336faeb2b000005a72bbd01fb8b
Masoud Azizi <mablue92@gmail.com>
2020-11-19 08:49:27 +0330

    Create ttc.py
    Initial script

---
+++ ttc.py
@@ -0,0 +1,16 @@
+# TTC: Twitter text compressing script
+# developer: Masoud Azizi
+# Email: mablue92@gmail.com
+i0="masoud azizi\nJafar AzizI \n amir mohammad AZIZI"
+print(i0)
+
+i1=i0.lower()
+o0=i1.split()
+print(o0)
+
+o2=[o1.capitalize() for o1 in o0]
+print(o2)
+
+o3="".join(o2)
+print("=======RESULT=======")
+print(o3)
